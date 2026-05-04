edde6b6d1aa15fb584a64a4fbb8f11863f8ef729
emmailiasova <emma.iliasova@piaristi-pd.sk>
2026-05-04 13:18:55 +0200

    neviem

---
+++ epu/09_podmienky/script.js
@@ -0,0 +1,39 @@
+Obsah obdlznika je:``
+Obvod obdlznika je:
+
+let stranaA = 9;
+let stranaB = 8;
+
+console.log (`obsah obdlznika je: ${stranaA} * ${stranaB} = ${stranaA * stranaB}`);
+
+console.log (`obvod obdlznika je: ${stranaA} a ${stranaB} je: ${2*stranaA + stranaB}`);
+
+
+Obsah kruhu je:
+Obvod kruhu je:
+
+let  polomer = 3;
+let  pi = 3,14;
+
+console.log (`obvod kruhu s polomerom ${polomer} je: ${pi*(polomer2)}`);
+
+console.log (`obsah kruhu s polomerom ${polomer} je: ${2*pi*polomer}`);
+
+
+let vek = 20;
+if (vek >= 18){
+    console.log("Môžeš šoférovať!")
+}
+
+let cenaNakupu = 101;
+if (cenaNakupu > 100){
+    console.log("Doprava zadarmo!");
+}
+
+if(cislo < 0){
+    console.log(`Cislo ${cislo} je zaporne`);
+}
+
+else{
+    console.log(`Cislo ${cislo} je kladne alebo nula`);
+}

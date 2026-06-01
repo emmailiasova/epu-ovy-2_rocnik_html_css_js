13fe8305de9c3a0259faec161a692610720134ad
emmailiasova <emma.iliasova@piaristi-pd.sk>
2026-06-01 13:18:22 +0200

    nove nieco

---
+++ epu/11_dom_udalosti/assets/js/script.js
@@ -0,0 +1,72 @@
+// ===================================
+// 1. VYTVOR PREMENNÉ
+// ===================================
+// Vytvor premenné s tvojimi údajmi:
+// meno, vek, mesto, konicek
+
+const meno = "Emma Iliašová ";
+
+// ===================================
+// 2. VÝPOČTY
+// ===================================
+// Vypočítaj rok narodenia (aktuálny rok 2025 - vek)
+
+
+// Vypočítaj koľko rokov ti chýba do 18 (18 - vek)
+
+const
+
+// ===================================
+// 3. ZOBRAZ ZÁKLADNÉ ÚDAJE
+// ===================================
+// Použi querySelector a textContent na zobrazenie:
+// - meno do elementu #meno
+// - vek do elementu #vek
+// - mesto do elementu #mesto
+// - konicek do elementu #konicek
+
+
+const ElementMeno = document.getElementById("meno");
+const ElementVek = document.getElementById("vek");
+const ElementMesto = document.getElementById("mesto");
+const ElementKonicek = document.getElementById("konicek");
+const ElementRokNarodenie = document.getElementById("rok-narodenia");
+const ElementDoDospelosti = document.getElementById("do-dospelosti");
+
+
+
+ElementMeno.textContent = meno;
+ElementVek.textContent = vek;
+ElementMesto.textContent = mesto;
+ElementKonicek.textContent = konicek;
+
+
+
+// ===================================
+// 4. ZOBRAZ VÝPOČTY
+// ===================================
+// Zobraz vypočítané hodnoty:
+// - rok narodenia do #rok-narodenia
+// - rokov do dospelosti do #do-dospelosti
+
+ElementRokNarodenia.textContent
+
+
+// ===================================
+// 5. VYTVOR BIO TEXT
+// ===================================
+// Použi template literals na vytvorenie vety:
+// "Ahoj! Volám sa [meno], mám [vek] rokov a pochádzam z [mesto]. Vo voľnom čase rád/-a [konicek]."
+// Zobraz text do elementu #o-mne
+
+
+
+// ===================================
+// 6. UPRAV ŠTÝLY CEZ JAVASCRIPT
+// ===================================
+// Najdi element #status a:
+// - Zmeň text na "Profil načítaný!"
+// - Odstráň triedu "nacitavam"
+// - Pridaj triedu "uspech"
+// - Zmeň farbu textu na #4CAF50
+
